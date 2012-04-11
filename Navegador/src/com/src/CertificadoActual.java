4b4905defea71c4c870d119977a2ae6851d27173
18fernan@gmail.com <18fernan@gmail.com@7288cb12-b090-320e-a024-a907365dbb7d>
2012-04-11 11:28:46 +0000

    Ventana del certificado actual creada

---
+++ Navegador/src/com/src/CertificadoActual.java
@@ -0,0 +1,13 @@
+package com.src;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class CertificadoActual extends SherlockActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
