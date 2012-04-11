50c54aec595c23e67ef1f954afb14797cf3a1494
ajrneo86@gmail.com <ajrneo86@gmail.com@7288cb12-b090-320e-a024-a907365dbb7d>
2012-04-11 11:55:43 +0000

    Metodos necesarios para acceder a Apache mediante SSL

--- Navegador/src/com/src/NavegadorActivity.java
+++ Navegador/src/com/src/NavegadorActivity.java
@@ -1,9 +1,36 @@
 
 package com.src;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.SingleClientConnManager;
+
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.webkit.WebView;
 
@@ -13,7 +40,8 @@ import com.actionbarsherlock.view.MenuItem;
 
 public class NavegadorActivity extends SherlockActivity {
     private WebView wv;
-    private static final String PROD_URL = "http://home.bacamt.com:83/cert/index.phtml";
+    private static final String PROD_URL = "https://home.bacamt.com:83/cert/showme.phtml";
+    private static final String LOGTAG = "https";
 
     /** Called when the activity is first created. */
     @Override
@@ -22,7 +50,8 @@ public class NavegadorActivity extends SherlockActivity {
         setContentView(R.layout.main);
         wv = (WebView) findViewById(R.id.webView1);
         wv.loadUrl(PROD_URL);
-        SharedPreferences settings = getSharedPreferences("certificado", MODE_PRIVATE);
+        SharedPreferences settings = getSharedPreferences("certificado",
+                MODE_PRIVATE);
         String nombre = settings.getString("nombre", " ");
         Log.d("MIO", "dentro de navegador " + nombre);
 
@@ -42,7 +71,7 @@ public class NavegadorActivity extends SherlockActivity {
         if (item.getItemId() == 0) {
             startActivity(new Intent(this, new ListaCertificados().getClass()));
         }
-        else if (item.getItemId()==1){
+        else if (item.getItemId() == 1) {
             startActivity(new Intent(this, CertificadoActual.class));
         }
         return h;
@@ -62,6 +91,84 @@ public class NavegadorActivity extends SherlockActivity {
                 // Handle successful scan
             } else if (resultCode == RESULT_CANCELED) {
             }
+        }
+    }
+
+    /* Metodo para generar certificados a partir del contenido de un String */
+    public void createCert() throws IOException {
+        String FILENAME = "cert";
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        final SharedPreferences.Editor editor = prefs.edit();
+
+        int numCerts = prefs.getInt("numCertificados", 1);
+        FILENAME += numCerts;
+        String string = "hello world!";
+
+        FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+        fos.write(string.getBytes());
+        fos.close();
+
+        editor.putInt("numCertificados", numCerts + 1);
+        editor.commit();
+    }
+
+    /* Metodo encargado de conectar con Apache mediante SSL */
+    public void connect() throws URISyntaxException, NoSuchAlgorithmException,
+            CertificateException, IOException, KeyStoreException, KeyManagementException,
+            UnrecoverableKeyException {
+
+        HttpGet request = new HttpGet(new URI(PROD_URL));
+
+        KeyStore trusted = KeyStore.getInstance("BKS");
+        trusted.load(getResources().openRawResource(R.raw.truststore), "inftel".toCharArray());
+
+        // De momento usamos el certificado directamente del raw. Posteriormente
+        // habra que cambiarlo
+        KeyStore clientCert = KeyStore.getInstance("pkcs12");
+        clientCert.load(getResources().openRawResource(R.raw.clientpass),
+                "inftel".toCharArray());
+
+        SSLSocketFactory sslf = new SSLSocketFactory(clientCert, null, trusted);
+        sslf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+        SchemeRegistry schemeRegistry = new SchemeRegistry();
+        schemeRegistry.register(new Scheme("https", sslf, 443));
+        SingleClientConnManager cm = new SingleClientConnManager(request.getParams(),
+                schemeRegistry);
+
+        HttpClient client = new DefaultHttpClient(cm, request.getParams());
+        HttpResponse result = client.execute(request);
+
+        /* Comprobamos que la respuesta que obtenemos es valida */
+        final int statusCode = result.getStatusLine().getStatusCode();
+        if (statusCode != HttpStatus.SC_OK) {
+            Log.w(LOGTAG, "Error " + statusCode + " for URL "
+                    + PROD_URL);
+        }
+        HttpEntity getResponseEntity = result.getEntity();
+        InputStream is = getResponseEntity.getContent();
+        Log.i(LOGTAG, "Content: " + convertinputStreamToString(is));
+
+    }
+
+    public String convertinputStreamToString(InputStream ists) throws IOException {
+
+        if (ists != null) {
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            try {
+                BufferedReader r1 = new BufferedReader(new InputStreamReader(ists, "UTF-8"));
+                while ((line = r1.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+            } finally {
+                ists.close();
+            }
+            return sb.toString();
+        } else {
+            return "";
         }
     }
 }
