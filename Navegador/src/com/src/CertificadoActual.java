1eef8b3cd48c3290357913a248b7e7d9feca9d61
18fernan@gmail.com <18fernan@gmail.com@7288cb12-b090-320e-a024-a907365dbb7d>
2012-04-12 12:06:39 +0000

    muestra el certificado

--- Navegador/src/com/src/CertificadoActual.java
+++ Navegador/src/com/src/CertificadoActual.java
@@ -1,13 +1,19 @@
 package com.src;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.Date;
+import java.util.Enumeration;
 
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockActivity;
@@ -22,18 +28,36 @@ public class CertificadoActual extends SherlockActivity {
         SharedPreferences settings = getSharedPreferences("certificado", MODE_PRIVATE);
         nombre = settings.getString("nombre", "");
         try {
-            FileInputStream fin = new FileInputStream(new File("/sdcard/certificados/"+nombre));
-            CertificateFactory cf = CertificateFactory.getInstance("X.509");
-            X509Certificate cert = (X509Certificate)cf.generateCertificate(fin);
-//            X509Certificate.getInstance(fin);
-            ((TextView)findViewById(R.id.textView11)).setText(cert.getIssuerDN().getName());
-            ((TextView)findViewById(R.id.textView21)).setText(cert.getSubjectDN().getName());
-            ((TextView)findViewById(R.id.textView31)).setText("");
-            ((TextView)findViewById(R.id.textView41)).setText("");
-            ((TextView)findViewById(R.id.textView51)).setText(cert.getNotBefore().toString());
-            ((TextView)findViewById(R.id.textView61)).setText(cert.getNotAfter().toString());
-            ((TextView)findViewById(R.id.textView71)).setText("");
-            ((TextView)findViewById(R.id.textView81)).setText(cert.getSigAlgOID());
+            KeyStore clientCert = KeyStore.getInstance("pkcs12");
+            FileInputStream fis = new FileInputStream(new File("/sdcard/certificados/" + nombre));
+            clientCert.load(fis, "inftel".toCharArray());
+            try{
+                Enumeration<String> es=clientCert.aliases();
+                String s = "";
+                while(es.hasMoreElements()){
+                    s=es.nextElement();
+                    Log.d("MIO", s);
+                }
+                X509Certificate x509=(X509Certificate) clientCert.getCertificate(s);
+                String issuerNombre=x509.getIssuerDN().getName();
+                issuerNombre=issuerNombre.substring(issuerNombre.indexOf("L"),issuerNombre.length());
+                String ciudad=issuerNombre.substring(issuerNombre.indexOf("L")+2, issuerNombre.indexOf(","));
+                issuerNombre=issuerNombre.substring(issuerNombre.indexOf("O"),issuerNombre.length());
+                String org=issuerNombre.substring(issuerNombre.indexOf("O")+2, issuerNombre.indexOf(","));
+                issuerNombre=issuerNombre.substring(issuerNombre.indexOf("CN"),issuerNombre.length());
+                String cn=issuerNombre.substring(issuerNombre.indexOf("CN")+3, issuerNombre.length());
+                ((TextView)findViewById(R.id.textView11)).setText(nombre);
+                ((TextView)findViewById(R.id.textView21)).setText(cn);
+                ((TextView)findViewById(R.id.textView31)).setText(org);
+                ((TextView)findViewById(R.id.textView41)).setText(cn);
+                ((TextView)findViewById(R.id.textView51)).setText(x509.getNotBefore().toGMTString());
+                ((TextView)findViewById(R.id.textView61)).setText(x509.getNotAfter().toGMTString());
+                ((TextView)findViewById(R.id.textView71)).setText(ciudad);
+                ((TextView)findViewById(R.id.textView81)).setText(x509.getSigAlgOID());
+            }
+            catch (Exception e) {
+                Log.d("MIO", "a saltado la excepcion" + nombre, e);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
